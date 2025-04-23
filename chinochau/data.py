71ae914e9d06bdae41adbf1a1579fa275e34f75b
Carlos García Ling <carlos.gling@hotmail.com>
2025-04-23 21:38:51 +0800

    Add basic deepseek connector

--- chinochau/data.py
+++ chinochau/data.py
@@ -1,5 +1,7 @@
+import os
 from dataclasses import dataclass
 from typing import List
+import pandas as pd
 
 
 @dataclass
@@ -8,3 +10,25 @@ class Flashcard:
     pinyin: str
     definitions: List[str] = None
     example: str = None
+
+
+class MasterFlashcards:
+    """This class will contain the master Flashcards the main goal
+    is to have caching of Flashcards to avoid reaching the Deepseek
+    endpoints too often therefore saving cost"""
+
+    def __init__(self, file="data/master.csv"):
+        if file[-4:] != ".csv":
+            raise ValueError("For the master datasource only '.csv' is supported")
+        self._file_path = file
+        if os.path.exists(file):
+            self.flashcards_dataframe = pd.read_csv(file)
+        else:
+            self.flashcards_dataframe = pd.DataFrame()
+
+    def import_flashcards(self, flashcards=List[Flashcard]):
+
+        new_data = pd.DataFrame([vars(card) for card in flashcards])
+
+    def save_flashcards(self):
+        self.flashcards_dataframe.to_csv(self._file_path)
--- chinochau/deepseek.py
+++ chinochau/deepseek.py
@@ -1,4 +1,31 @@
+from openai import OpenAI
+
+
 with open("api_key.txt", "r") as f:
     key = f.read()
 
-print(key)
+client = OpenAI(api_key=key, base_url="https://api.deepseek.com")
+
+
+def get_examples_deepseek(word: str, number_of_examples: int = 2):
+
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+                "role": "system",
+                "content": f"你是一位中文教师，面向HSK4水平的学生。当学生给出一个词语时，你需用中文回复{number_of_examples}个不同的例句来演示该词的用法。每个例句单独一行，不要编号。不要使用拼音、英语或任何额外解释，仅提供符合HSK4水平的例句",
+            },
+            {"role": "user", "content": word},
+        ],
+        stream=False,
+    )
+
+    return response.choices[0].message.content
+
+
+if __name__ == "__main__":
+    sample_word = "提供"
+    example = get_examples_deepseek(sample_word)
+    print(sample_word)
+    print(example)
